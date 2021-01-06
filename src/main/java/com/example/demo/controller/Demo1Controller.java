b83c99d3f1657473a25b793c2750626e2412d328
yangyiyang_dxm <yangyiyang@duxiaoman.com>
2021-01-06 16:14:49 +0800

    fix

--- src/main/java/com/example/demo/controller/Demo1Controller.java
+++ src/main/java/com/example/demo/controller/Demo1Controller.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,8 +10,31 @@ public class Demo1Controller {
 
     }
 
-//    @RequestMapping("/hello")
-//    public String hello() {
-//        return "Hello Spring Boot!";
-//    }
+    @RequestMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        int n = 12;
+        Integer num = countDigitOne(n);
+        return num.toString();
+    }
+
+    private int countDigitOne(int n) {
+        int digit = 1, res = 0;
+        int high = n / 10, cur = n % 10, low = 0;
+
+        while (high != 0 || cur != 0) {
+            if (cur == 0) {
+                res += high * digit;
+            } else if (cur == 1) {
+                res += high * digit + low + 1;
+            } else {
+                res += (high + 1) * digit;
+            }
+
+            low += cur * digit;
+            cur = high % 10;
+            high /= 10;
+            digit *= 10;
+        }
+        return res;
+    }
 }
